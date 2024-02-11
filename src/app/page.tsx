38a40d101dcc5cf06663447998f701486cb65e52
vinh <onehitdragon@gmail.com>
2024-02-11 08:41:16 +0700

    change download files to 0.0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
         <DownloaderList downloaders={[
             {
                 platformName: "Window 64bit Installer",
-                fileName: "Godgon-0.0.1-dev.exe",
-                url: "/apps/godgon-0.0.1-dev.exe"
+                fileName: "Godgon-0.0.2-dev.exe",
+                url: "/apps/godgon-0.0.2-dev.exe"
             },
             {
                 platformName: "Window 64bit Zip",
-                fileName: "Godgon-0.0.1-dev.zip",
-                url: "/apps/godgon-0.0.1-dev.zip"
+                fileName: "Godgon-0.0.2-dev.zip",
+                url: "/apps/godgon-0.0.2-dev.zip"
             },
             {
                 platformName: "Android",
-                fileName: "Godgon-0.0.1-dev.apk",
-                url: "/apps/godgon-0.0.1-dev.apk"
+                fileName: "Godgon-0.0.2-dev.apk",
+                url: "/apps/godgon-0.0.2-dev.apk"
             }
         ]} />
     )
